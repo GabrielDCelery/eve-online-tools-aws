7d27b7f0454c1595ba62b2fa9d5585464b77eedf
GabrielDCelery <gabriel.d.celery@gmail.com>
2023-10-29 09:40:59 +0000

    added type auto generation

--- services/market-orders-downloader/dev/data-types-generator.js
+++ services/market-orders-downloader/dev/data-types-generator.js
@@ -1,13 +1,27 @@
-import { compileFromFile } from 'json-schema-to-typescript';
+const { compileFromFile } = require('json-schema-to-typescript');
 const path = require('path');
 const fs = require('fs');
 
 (async () => {
-    const fileNames = ['eve-api-market-order'];
+    const schemas = [
+        {
+            name: 'eve-api-market-order',
+            version: '1.0.0',
+        },
+    ];
 
-    for (let i = 0, iMax = fileNames.length; i < iMax; i++) {
-        const ts = await compileFromFile(path.join(__dirname, '../../../schemas', `${fileNames[i]}.json`), { additionalProperties: false });
-        fs.writeFileSync(path.join(__dirname, '../src/data-types', `${fileNames[i]}.ts`), ts);
+    for (let i = 0, iMax = schemas.length; i < iMax; i++) {
+        const schema = schemas[i];
+        const schemaPath = path.join(__dirname, '../../../schemas', `${schema.name}`, `${schema.name}-${schema.version}.json`);
+        const ts = await compileFromFile(schemaPath, { additionalProperties: false });
+
+        const outputDir = path.join(__dirname, '../src/data-types/generated', `${schema.name}`);
+
+        if (!fs.existsSync(outputDir)) {
+            fs.mkdirSync(outputDir, { recursive: true });
+        }
+
+        fs.writeFileSync(path.join(outputDir, `${schema.name}-${schema.version}.ts`), ts);
     }
 })();
 
--- services/market-orders-downloader/src/get-market-orders.ts
+++ services/market-orders-downloader/src/get-market-orders.ts
@@ -1,10 +1,10 @@
 import axios, { AxiosError } from 'axios';
-import { EveAPIMarketOrder } from './data-types/eve-api-market-order';
+import { EveAPI__MarketOrder__1_0_0 } from './data-types/generated/eve-api-market-order/eve-api-market-order-1.0.0';
 
-export const getMarketOrders = async ({ sectorID, page }: { sectorID: number; page: number }): Promise<EveAPIMarketOrder[]> => {
+export const getMarketOrders = async ({ sectorID, page }: { sectorID: number; page: number }): Promise<EveAPI__MarketOrder__1_0_0[]> => {
     try {
         const url = `https://esi.evetech.net/latest/markets/${sectorID}/orders/?datasource=tranquility&order_type=all&page=${page}`;
-        const response = await axios.get<EveAPIMarketOrder[]>(url);
+        const response = await axios.get<EveAPI__MarketOrder__1_0_0[]>(url);
         return response.data;
     } catch (error_) {
         const error = error_ as AxiosError;

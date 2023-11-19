18a32b090251e76b2a5a89e358da047377f459b8
GabrielDCelery <gabriel.d.celery@gmail.com>
2023-11-19 12:55:06 +0000

    custom resource for ASG EC2 replacement

--- aws-infrastructure/core/lib/l3/eve-online-tools-vpc.ts
+++ aws-infrastructure/core/lib/l3/eve-online-tools-vpc.ts
@@ -1,7 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
-import * as iam from 'aws-cdk-lib/aws-iam';
+import * as aws_cdk from 'aws-cdk-lib';
+import * as aws_ec2 from 'aws-cdk-lib/aws-ec2';
+import * as aw_autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as aws_iam from 'aws-cdk-lib/aws-iam';
+import * as aws_logs from 'aws-cdk-lib/aws-logs';
+import * as aws_cr from 'aws-cdk-lib/custom-resources';
+import * as uuid from 'uuid';
 import { Construct } from 'constructs';
 
 export interface EveOnlineToolsVpcConstructProps {
@@ -11,7 +14,7 @@ export interface EveOnlineToolsVpcConstructProps {
 }
 
 export class EveOnlineToolsVpcConstruct extends Construct {
-    public vpc: ec2.Vpc;
+    public vpc: aws_ec2.Vpc;
 
     constructor(scope: Construct, id: string, props: EveOnlineToolsVpcConstructProps) {
         super(scope, id);
@@ -21,26 +24,26 @@ export class EveOnlineToolsVpcConstruct extends Construct {
         this.vpc = vpc;
     }
 
-    private createVpc({ props }: { props: EveOnlineToolsVpcConstructProps }): ec2.Vpc {
-        const vpc = new ec2.Vpc(this, 'Vpc', {
-            ipAddresses: ec2.IpAddresses.cidr(props.cidrBlock),
+    private createVpc({ props }: { props: EveOnlineToolsVpcConstructProps }): aws_ec2.Vpc {
+        const vpc = new aws_ec2.Vpc(this, 'Vpc', {
+            ipAddresses: aws_ec2.IpAddresses.cidr(props.cidrBlock),
             maxAzs: 2,
             natGateways: 0,
             subnetConfiguration: [
                 {
                     cidrMask: 19,
                     name: 'Public',
-                    subnetType: ec2.SubnetType.PUBLIC,
+                    subnetType: aws_ec2.SubnetType.PUBLIC,
                 },
                 {
                     cidrMask: 19,
                     name: 'Private',
-                    subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                    subnetType: aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
                 },
                 {
                     cidrMask: 19,
                     name: 'Isolated',
-                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                    subnetType: aws_ec2.SubnetType.PRIVATE_ISOLATED,
                 },
             ],
             vpcName: props.vpcName,
@@ -49,57 +52,55 @@ export class EveOnlineToolsVpcConstruct extends Construct {
         return vpc;
     }
 
-    private createCustomNATGateway({ vpc, props }: { vpc: ec2.Vpc; props: EveOnlineToolsVpcConstructProps }) {
-        const natGatewaySecurityGroup = new ec2.SecurityGroup(this, 'VpcNatGatewaySecurityGroup', {
+    private createCustomNATGateway({ vpc, props }: { vpc: aws_ec2.Vpc; props: EveOnlineToolsVpcConstructProps }) {
+        const natGatewaySecurityGroup = new aws_ec2.SecurityGroup(this, 'VpcNatGatewaySecurityGroup', {
             vpc: vpc,
             description: 'Custom Vpc Nat gateway security group',
             allowAllOutbound: false,
         });
 
-        natGatewaySecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
-
         vpc.privateSubnets.forEach((privateSubnet) => {
-            natGatewaySecurityGroup.addIngressRule(ec2.Peer.ipv4(privateSubnet.ipv4CidrBlock), ec2.Port.tcp(80));
-            natGatewaySecurityGroup.addIngressRule(ec2.Peer.ipv4(privateSubnet.ipv4CidrBlock), ec2.Port.tcp(443));
+            natGatewaySecurityGroup.addIngressRule(aws_ec2.Peer.ipv4(privateSubnet.ipv4CidrBlock), aws_ec2.Port.tcp(80));
+            natGatewaySecurityGroup.addIngressRule(aws_ec2.Peer.ipv4(privateSubnet.ipv4CidrBlock), aws_ec2.Port.tcp(443));
         });
 
-        natGatewaySecurityGroup.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
-        natGatewaySecurityGroup.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
+        natGatewaySecurityGroup.addEgressRule(aws_ec2.Peer.anyIpv4(), aws_ec2.Port.tcp(80));
+        natGatewaySecurityGroup.addEgressRule(aws_ec2.Peer.anyIpv4(), aws_ec2.Port.tcp(443));
 
-        const natGatewayInstanceRole = new iam.Role(this, 'NatGatewayInstanceRole', {
-            assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-            inlinePolicies: {
-                toExecuteUserData: new iam.PolicyDocument({
-                    statements: [
-                        new iam.PolicyStatement({
-                            effect: iam.Effect.ALLOW,
-                            actions: ['ec2:DescribeNetworkInterfaces'],
-                            resources: ['*'],
-                        }),
-                        new iam.PolicyStatement({
-                            effect: iam.Effect.ALLOW,
-                            actions: ['ec2:CreateRoute', 'ec2:ReplaceRoute'],
-                            resources: ['*'],
-                        }),
-                        new iam.PolicyStatement({
-                            effect: iam.Effect.ALLOW,
-                            actions: ['ec2:ModifyNetworkInterfaceAttribute'],
-                            resources: ['*'],
-                        }),
-                    ],
+        const natGatewayUserDataPolicy = new aws_iam.ManagedPolicy(this, 'NatGatewayUserDataPolicy', {
+            statements: [
+                new aws_iam.PolicyStatement({
+                    effect: aws_iam.Effect.ALLOW,
+                    actions: ['ec2:DescribeNetworkInterfaces'],
+                    resources: ['*'],
                 }),
-            },
+                new aws_iam.PolicyStatement({
+                    effect: aws_iam.Effect.ALLOW,
+                    actions: ['ec2:CreateRoute', 'ec2:ReplaceRoute'],
+                    resources: ['*'],
+                }),
+                new aws_iam.PolicyStatement({
+                    effect: aws_iam.Effect.ALLOW,
+                    actions: ['ec2:ModifyNetworkInterfaceAttribute'],
+                    resources: ['*'],
+                }),
+            ],
         });
 
-        const natGatewayLaunchTemplate = new ec2.LaunchTemplate(this, 'NatGatewayLaunchTemplate', {
-            instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-            machineImage: ec2.MachineImage.genericLinux({
+        const natGatewayInstanceRole = new aws_iam.Role(this, 'NatGatewayInstanceRole', {
+            assumedBy: new aws_iam.ServicePrincipal('ec2.amazonaws.com'),
+            managedPolicies: [natGatewayUserDataPolicy],
+        });
+
+        const natGatewayLaunchTemplate = new aws_ec2.LaunchTemplate(this, 'NatGatewayLaunchTemplate', {
+            instanceType: aws_ec2.InstanceType.of(aws_ec2.InstanceClass.T3A, aws_ec2.InstanceSize.NANO),
+            machineImage: aws_ec2.MachineImage.genericLinux({
                 'eu-west-2': 'ami-02719b84cb731adfa',
             }),
             securityGroup: natGatewaySecurityGroup,
             associatePublicIpAddress: true,
             userData: (() => {
-                const userData = ec2.UserData.forLinux();
+                const userData = aws_ec2.UserData.forLinux();
                 userData.addCommands(
                     `yum update -y aws-cfn-bootstrap`,
                     `yum install -y jq`,
@@ -112,10 +113,9 @@ export class EveOnlineToolsVpcConstruct extends Construct {
                 return userData;
             })(),
             role: natGatewayInstanceRole,
-            keyName: 'gzeller_nas',
         });
 
-        new autoscaling.AutoScalingGroup(this, 'NatGatewayAutoScalingGroup', {
+        const autoScalingGroup = new aw_autoscaling.AutoScalingGroup(this, 'NatGatewayAutoScalingGroup', {
             vpc: vpc,
             vpcSubnets: {
                 // subnetType: ec2.SubnetType.PUBLIC,
@@ -124,9 +124,60 @@ export class EveOnlineToolsVpcConstruct extends Construct {
             launchTemplate: natGatewayLaunchTemplate,
             minCapacity: 1,
             maxCapacity: 1,
-            healthCheck: autoscaling.HealthCheck.ec2({
-                grace: cdk.Duration.minutes(3),
+            healthCheck: aw_autoscaling.HealthCheck.ec2({
+                grace: aws_cdk.Duration.minutes(2),
             }),
         });
+
+        const onCreate: aws_cr.AwsSdkCall = {
+            service: 'AutoScaling',
+            action: 'startInstanceRefresh',
+            parameters: {
+                AutoScalingGroupName: autoScalingGroup.autoScalingGroupName,
+            },
+            /*
+            parameters: {
+                AutoScalingGroupName: autoScalingGroup.autoScalingGroupName,
+                DesiredConfiguration: {
+                    LaunchTemplate: {
+                        LaunchTemplateId: natGatewayLaunchTemplate.launchTemplateId,
+                        Version: '$Latest',
+                    },
+                },
+                Preferences: {
+                    AutoRollback: true,
+                },
+            },
+            */
+            physicalResourceId: aws_cr.PhysicalResourceId.of(`startInstanceRefresh:${autoScalingGroup.autoScalingGroupName}`),
+        };
+
+        // const a = aws_iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEC2FullAccess');
+
+        const customResource = new aws_cr.AwsCustomResource(this, `NatGatewayInstanceRefresh${uuid.v4().replace(/-/g, '')}`, {
+            policy: aws_cr.AwsCustomResourcePolicy.fromStatements([
+                new aws_iam.PolicyStatement({
+                    effect: aws_iam.Effect.ALLOW,
+                    resources: ['*'],
+                    actions: ['autoscaling:StartInstanceRefresh'],
+                }),
+                /*
+                new aws_iam.PolicyStatement({
+                    effect: aws_iam.Effect.ALLOW,
+                    resources: ['*'],
+                    actions: ['ec2:DescribeLaunchTemplates'],
+                }),
+                new aws_iam.PolicyStatement({
+                    effect: aws_iam.Effect.ALLOW,
+                    resources: ['*'],
+                    actions: ['ec2:DescribeLaunchTemplateVersions'],
+                }),
+                */
+            ]),
+            logRetention: aws_logs.RetentionDays.ONE_DAY,
+            onCreate,
+        });
+
+        customResource.node.addDependency(autoScalingGroup);
     }
 }
--- aws-infrastructure/core/lib/l3/internal-vpc-peering.ts
+++ aws-infrastructure/core/lib/l3/internal-vpc-peering.ts
@@ -1,12 +1,12 @@
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import * as logs from 'aws-cdk-lib/aws-logs';
-import { AwsCustomResource, AwsCustomResourcePolicy, AwsSdkCall, PhysicalResourceId } from 'aws-cdk-lib/custom-resources';
+import * as aws_ec2 from 'aws-cdk-lib/aws-ec2';
+import * as aws_iam from 'aws-cdk-lib/aws-iam';
+import * as aws_logs from 'aws-cdk-lib/aws-logs';
+import * as aws_cr from 'aws-cdk-lib/custom-resources';
 import { Construct } from 'constructs';
 
 export interface InternalVpcPeeringConstructProps {
-    requesterVpc: ec2.Vpc;
-    accepterVpc: ec2.Vpc;
+    requesterVpc: aws_ec2.Vpc;
+    accepterVpc: aws_ec2.Vpc;
     accepterOwnerId: string;
     accepterRegion: string;
 }
@@ -20,8 +20,8 @@ export class InternalVpcPeeringConstruct extends Construct {
         this.enablePeeringDNSResolution({ peeringConnection, props });
     }
 
-    private createPeeringConnection({ props }: { props: InternalVpcPeeringConstructProps }): ec2.CfnVPCPeeringConnection {
-        const peeringConnection = new ec2.CfnVPCPeeringConnection(this, `Connection`, {
+    private createPeeringConnection({ props }: { props: InternalVpcPeeringConstructProps }): aws_ec2.CfnVPCPeeringConnection {
+        const peeringConnection = new aws_ec2.CfnVPCPeeringConnection(this, `Connection`, {
             vpcId: props.requesterVpc.vpcId,
             peerVpcId: props.accepterVpc.vpcId,
             peerOwnerId: props.accepterOwnerId,
@@ -36,9 +36,9 @@ export class InternalVpcPeeringConstruct extends Construct {
         return peeringConnection;
     }
 
-    private createRoutesFromRequesterToAccepter({ peeringConnection, props }: { peeringConnection: ec2.CfnVPCPeeringConnection; props: InternalVpcPeeringConstructProps }) {
+    private createRoutesFromRequesterToAccepter({ peeringConnection, props }: { peeringConnection: aws_ec2.CfnVPCPeeringConnection; props: InternalVpcPeeringConstructProps }) {
         props.requesterVpc.privateSubnets.forEach(({ routeTable: { routeTableId } }, index) => {
-            const route = new ec2.CfnRoute(this, 'PrivateSubnetPeeringConnectionRoute' + index, {
+            const route = new aws_ec2.CfnRoute(this, 'PrivateSubnetPeeringConnectionRoute' + index, {
                 destinationCidrBlock: props.accepterVpc.vpcCidrBlock,
                 routeTableId,
                 vpcPeeringConnectionId: peeringConnection.ref,
@@ -47,8 +47,8 @@ export class InternalVpcPeeringConstruct extends Construct {
         });
     }
 
-    private enablePeeringDNSResolution({ peeringConnection }: { peeringConnection: ec2.CfnVPCPeeringConnection; props: InternalVpcPeeringConstructProps }) {
-        const onCreate: AwsSdkCall = {
+    private enablePeeringDNSResolution({ peeringConnection }: { peeringConnection: aws_ec2.CfnVPCPeeringConnection; props: InternalVpcPeeringConstructProps }) {
+        const onCreate: aws_cr.AwsSdkCall = {
             service: 'EC2',
             action: 'modifyVpcPeeringConnectionOptions',
             parameters: {
@@ -60,10 +60,10 @@ export class InternalVpcPeeringConstruct extends Construct {
                     AllowDnsResolutionFromRemoteVpc: true,
                 },
             },
-            physicalResourceId: PhysicalResourceId.of(`enableVPCPeeringDNSResolution:${peeringConnection.ref}`),
+            physicalResourceId: aws_cr.PhysicalResourceId.of(`enableVPCPeeringDNSResolution:${peeringConnection.ref}`),
         };
         const onUpdate = onCreate;
-        const onDelete: AwsSdkCall = {
+        const onDelete: aws_cr.AwsSdkCall = {
             service: 'EC2',
             action: 'modifyVpcPeeringConnectionOptions',
             parameters: {
@@ -77,15 +77,15 @@ export class InternalVpcPeeringConstruct extends Construct {
             },
         };
 
-        const customResource = new AwsCustomResource(this, 'EnablePeeringDNSResolution', {
-            policy: AwsCustomResourcePolicy.fromStatements([
-                new iam.PolicyStatement({
-                    effect: iam.Effect.ALLOW,
+        const customResource = new aws_cr.AwsCustomResource(this, 'EnablePeeringDNSResolution', {
+            policy: aws_cr.AwsCustomResourcePolicy.fromStatements([
+                new aws_iam.PolicyStatement({
+                    effect: aws_iam.Effect.ALLOW,
                     resources: ['*'],
                     actions: ['ec2:ModifyVpcPeeringConnectionOptions'],
                 }),
             ]),
-            logRetention: logs.RetentionDays.ONE_DAY,
+            logRetention: aws_logs.RetentionDays.ONE_DAY,
             onCreate,
             onUpdate,
             onDelete,

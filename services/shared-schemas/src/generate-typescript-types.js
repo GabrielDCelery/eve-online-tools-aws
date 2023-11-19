f2d29a3852db9f5a7ce98c180e53c0954a882039
GabrielDCelery <gabriel.d.celery@gmail.com>
2023-11-19 14:59:19 +0000

    Moved around schema generator

---
+++ services/shared-schemas/src/generate-typescript-types.js
@@ -0,0 +1,38 @@
+const { compileFromFile } = require("json-schema-to-typescript");
+const path = require("path");
+const fs = require("fs");
+
+(async () => {
+  const schemas = [
+    {
+      name: "eve-api-market-order",
+      version: "1.0.0",
+    },
+  ];
+
+  for (let i = 0, iMax = schemas.length; i < iMax; i++) {
+    const schema = schemas[i];
+    const schemaPath = path.join(
+      __dirname,
+      `${schema.name}`,
+      `${schema.name}-${schema.version}.json`
+    );
+
+    const ts = await compileFromFile(schemaPath, {
+      additionalProperties: false,
+    });
+
+    const outputDir = path.join(__dirname, "../generated");
+
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    fs.writeFileSync(
+      path.join(outputDir, `${schema.name}-${schema.version}.ts`),
+      ts
+    );
+  }
+})();
+
+// or, compile a JS object
